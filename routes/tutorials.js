a1af9917c073920fd3e74a34741932b41e75b137
YikaJ <547179699@qq.com>
2014-12-04 20:32:29 +0800

    分页完成

--- routes/tutorials.js
+++ routes/tutorials.js
@@ -8,7 +8,10 @@ var markdown = require("markdown").markdown;
 var articleTitle = "";
 var pubTime = "";
 var post = "";
-var firstArticle = [];
+var prePage = "";
+var nextPage = "";
+var listArticles = "";
+var pagePath = "";
 var main_href = "/tutorials/main";
 var node_href = "/tutorials/node";
 var express_href = "/tutorials/express";
@@ -16,8 +19,33 @@ var ejs_href = "/tutorials/ejs";
 var mongodb_href = "/tutorials/mongodb";
 
 router.get(/^\/*/, function(req, res, next){
-	var type = req.path.slice(1);
-	Article.findOne({type: type}, function(err, article){
+	/* 自己挖的坑！！！让你大小写随便写！ */
+	var type = "";
+	switch(req.path.slice(1)){
+		case "node":
+			type = "Node";
+			break;
+		case "express":
+			type = "Express";
+			break;
+		case "ejs":
+			type = "Ejs";
+			break;
+		case "mongodb":
+			type = "MongoDB";
+			break;
+		default:
+			type = "main";
+	}
+	/* 实现分页 */
+	var page = req.query["page"]? req.query["page"] : 0;
+
+	/* 这里是第一篇的意思 */
+	Article.find({type: type}, function(err, article){
+			listArticles = article;
+			var len = article.length - 1;
+			var article = article[page];
+			console.log(len);
 			if(err){
 				return console.log(err);
 			}
@@ -25,13 +53,19 @@ router.get(/^\/*/, function(req, res, next){
 				articleTitle = "尚未添加";
 				pubTime = "尚未添加";
 				post = "尚未添加";
+				prePage = "";
+				nextPage = "";
 			}else{
+				pagePath = "/tutorials" + req.path + "?page=";
 				post = markdown.toHTML(article.article);
 				articleTitle = article.title;
 				pubTime = article.date.month + " " + article.date.date + ", " +  article.date.year;
+				prePage = (page == 0)? "" : pagePath + (parseInt(page,10) - 1);
+		 		nextPage = (page == len)? "" : pagePath + (parseInt(page,10) + 1);
 			}
 			next();
 	})
+
 })
 
 /*这里学到了，render的第一个参数是模版的位置，也就是view层的位置，非地址*/
@@ -44,6 +78,10 @@ router.get("/main", function(req, res){
 				articleTitle: articleTitle,
 				pubTime: pubTime,
 				article: post,
+				prePage: prePage,
+				nextPage: nextPage,
+				listArticles: listArticles,
+				pagePath: pagePath,
 				main_href: main_href,
 				node_href: node_href,
 				express_href: express_href,
@@ -60,6 +98,10 @@ router.get("/node", function(req, res){
 				articleTitle: articleTitle,
 				pubTime: pubTime,
 				article: post,
+				prePage: prePage,
+				nextPage: nextPage,
+				listArticles: listArticles,
+				pagePath: pagePath,
 				main_href: main_href,
 				node_href: node_href,
 				express_href: express_href,
@@ -76,6 +118,10 @@ router.get("/express", function(req, res){
 				articleTitle: articleTitle,
 				pubTime: pubTime,
 				article: post,
+				prePage: prePage,
+				nextPage: nextPage,
+				listArticles: listArticles,
+				pagePath: pagePath,
 				main_href: main_href,
 				node_href: node_href,
 				express_href: express_href,
@@ -92,6 +138,10 @@ router.get("/ejs", function(req, res){
 				articleTitle: articleTitle,
 				pubTime: pubTime,
 				article: post,
+				prePage: prePage,
+				nextPage: nextPage,
+				listArticles: listArticles,
+				pagePath: pagePath,
 				main_href: main_href,
 				node_href: node_href,
 				express_href: express_href,
@@ -108,6 +158,10 @@ router.get("/mongodb", function(req, res){
 				articleTitle: articleTitle,
 				pubTime: pubTime,
 				article: post,
+				prePage: prePage,
+				nextPage: nextPage,
+				listArticles: listArticles,
+				pagePath: pagePath,
 				main_href: main_href,
 				node_href: node_href,
 				express_href: express_href,
